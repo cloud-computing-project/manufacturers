5afe601db1b41c2ffe79e6a5f61dd45d1e6763ea
Amra <ao2282@student.uni-lj.si>
2018-01-07 18:30:19 +0100

    fixed ManufacturersDS

--- models/src/main/resources/sql-scripts/init-manufacturers.sql
+++ models/src/main/resources/sql-scripts/init-manufacturers.sql
@@ -1,3 +1,3 @@
-INSERT INTO manufacturers (id,title, country) VALUES (1, 'Zara', 'Spain');
-INSERT INTO manufacturers (id,title, country) VALUES (2, 'H&M', 'Sweden' );
-INSERT INTO manufacturers (id,title, country) VALUES (3, 'Prada', 'Italy');
+INSERT INTO manufacturer (id,title, country) VALUES (1, 'Zara', 'Spain');
+INSERT INTO manufacturer (id,title, country) VALUES (2, 'H&M', 'Sweden' );
+INSERT INTO manufacturer (id,title, country) VALUES (3, 'Prada', 'Italy');
