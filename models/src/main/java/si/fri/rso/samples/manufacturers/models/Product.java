15d88074f7a5bb57ed51397ae3b5767f6b4a6a4c
Amela Špica <amela.spica@gmail.com>
2018-01-18 21:40:09 +0100

    etcd edit product

--- models/src/main/java/si/fri/rso/samples/manufacturers/models/Product.java
+++ models/src/main/java/si/fri/rso/samples/manufacturers/models/Product.java
@@ -1,12 +1,31 @@
 package si.fri.rso.samples.manufacturers.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 public class Product{
 
+    @Id
+    @GeneratedValue(generator = "idGenerator")
     private String id;
+
     private String title;
+
+    @Column(name = "manufacturer_id")
     private String manufacturerId;
+
+    @Column(name = "itemspecific_id")
+    private String itemSpecificId;
+
+    @Column(name = "category_id")
+    private String categoryId;
+
+    private String price;
+
+    @Column(name = "returnpolicy_id")
+    private String returnPolicyId;
 
     public String getId() {
         return id;
@@ -30,5 +49,37 @@ public class Product{
 
     public void setManufacturerId(String manufacturerId) {
         this.manufacturerId = manufacturerId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getReturnPolicyId() {
+        return returnPolicyId;
+    }
+
+    public void setReturnPolicyId(String returnPolicyId) {
+        this.returnPolicyId = returnPolicyId;
+    }
+
+    public String getItemSpecificId() {
+        return itemSpecificId;
+    }
+
+    public void setItemSpecificId(String itemSpecificId) {
+        this.itemSpecificId = itemSpecificId;
     }
 }
